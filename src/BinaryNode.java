cb2dfca478178907d69b06479689897fa8ac14ca
ericz <ericz@10.0.0.89>
2024-01-13 23:36:31 -0600

    bn

--- src/BinaryNode.java
+++ src/BinaryNode.java
@@ -1,4 +1,28 @@
-
 public class BinaryNode {
-
+	private BinaryNode left, right;
+	private Comparable myValue;
+	
+	public BinaryNode(Comparable x) {
+		myValue = x;
+		left = null;
+		right = null;
+	}
+	
+	public void setLeft(BinaryNode bn) {
+		left = bn;
+	}
+	public void setRight(BinaryNode bn) {
+		right = bn;
+	}
+	
+	public BinaryNode left() {
+		return left;
+	}
+	public BinaryNode right() {
+		return right;
+	}
+	
+	public Comparable getValue() {
+		return myValue;
+	}
 }
