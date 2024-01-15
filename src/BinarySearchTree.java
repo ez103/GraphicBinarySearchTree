586d0e734a5c5eb4a5571db0c622b5ba6520e607
ericz <ericz@10.0.0.89>
2024-01-15 00:08:11 -0600

    make class

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,4 +1,59 @@
 
 public class BinarySearchTree {
-
+	public BinaryNode root;
+	
+	public Comparable[] level0;
+	public Comparable[] level1;
+	public Comparable[] level2;
+	public Comparable[] level3;
+	public Comparable[] level4;
+	public Comparable[] level5;
+	
+	public BinarySearchTree() {
+		root = null;
+	}
+	
+	public void add(BinaryNode x) {
+		if (root == null) {
+			root = x;
+		}
+		else {
+			add(root, x);
+		}
+	}
+	public void add(BinaryNode parent, BinaryNode x) {
+		if(parent == null) return;
+		if(x.getValue().compareTo(parent.getValue()) < 0) {
+			if(parent.left() == null) {
+				parent.setLeft(x);
+			}
+			else {
+				add(parent.left(), x);
+			}
+		}
+		else {
+			if(parent.right() == null) {
+				parent.setRight(x);
+			}
+			else {
+				add(parent.right(), x);
+			}
+		}
+	}
+	
+	public void fillLevels() {
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
